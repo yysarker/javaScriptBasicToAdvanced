253ca63a1aea2bda7215f2f6a3188f3c0ca4d63a
= <=>
2022-01-14 21:05:49 +0600

    Output ways for javaScript

--- script.js
+++ script.js
@@ -1,1 +1,63 @@
-document.getElementById("demo").innerHTML= "Welcome to JavaScript Basic to Advanced";
+//Arithmetic Operator
+
+// + - * / % ++ --
+
+var a = 11;
+var b = 6;
+var c = a +b;
+console.log(a ++);
+console.log(a)
+
+
+
+
+/*
+===========
+JavaScript Display possibilities
+===========
+
+innerHTML()
+document.write()
+window.alert()
+console.log()
+
+
+innerHTML()
+document.write()
+windows.alert()
+console.log()
+
+
+*/
+
+// Using InnerHTML() 
+// document.getElementById("chapter2").innerHTML = "We learn about JavaScript Operators  a + b = "+c;
+
+//Using document.write()
+// "This only used for testing, because it delete existing data during it loaded"
+// document.write(5 + 6);
+
+
+//Using windows.alert()
+
+/*
+You can use an alert box to display data:
+*/
+
+// alert("Yeahyea Sarker !");
+
+//Using console.log()
+
+/*
+For debugging purposes, you can call the console.log() method in the browser to display data.
+*/
+
+console.log("Hello World")
+
+//JavaScript Print
+
+/*
+JavaScript does not have any print object or print methods. You cannot access output devices from JavaScript.
+The only exception is call the window.print() method in the browser to print the content of the current window.
+*/
+
