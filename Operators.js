60f50b39f07f5d3c64cd74bafed4357bed54a230
= <=>
2022-01-16 21:09:02 +0600

    Complete JS Operators

---
+++ Operators.js
@@ -0,0 +1,89 @@
+//JavaScript Operators
+
+/**
+ * Assignment values to variables and add them together:
+ * 
+ * let x = 5;
+ * let y = 6;
+ * let z = x + y;
+ * console.log(z); 
+ */
+
+// The assignment operator ( = ) assigns a value to a variable.
+
+// Assignment
+
+let x = 10;
+
+/**
+ * JavaScript Arithmetic Operators
+ * = // It's Assignment ( = )
+ * + //It's Addition ( + )
+ * * // It's Multiplication ( * )
+ * / Division ( /  )
+ * % Modulus ( % )
+ * ++ Increment ( + )
+ * -- Decrement ( - )
+ * ** Exponentiation ( ** ) // It's add into ES2016
+ */
+
+let a = 5;
+console.log(a**2);
+
+//Adding Strings and Number
+
+/**
+ * Adding two numbers, will return the sum, but adding a number and string will return a string:
+ * let x = 5 + 5;
+ * let y = "5" + 5;
+ * let z = "Hello" + 5;
+ * Output:
+ * console.log(x);
+ * console.log(y);
+ * console.log(z);
+ */
+
+//JavaScript Comparison Operators
+//Note: (=) This is not Comparison Operators, this is assignment operator
+/**
+ * Equal to (==)
+ * Equal value and Equal type (===)
+ * Not Equal (!=)
+ * Not equal value and not equal type (!==)
+ * Grater than (>)
+ * Less than (<)
+ * Greater than or equal to (>=)
+ * Less than or equal to (<=)
+ * Ternary Operator (?) 
+ */
+
+//JavaScript Logical Operators
+
+/**
+ * Logical and ( && )
+ * Logical or ( || )
+ * Logical not ( ! )
+ */
+
+//Type Operators
+/**
+ * typeof // Returns the type of a variable
+ * instanceof // Return true if an object is an instance of an object type
+ */
+
+//JavaScript Bitwise Operators
+/**
+ * Bit operators work on 32 bits numbers.
+ * 
+ * Any numeric operand in the operation is converted into a 32 bit number. The result is converted back to a JavaScript number.
+ * 
+ * 
+ * ( & ) --> AND
+ * ( | ) --> OR 
+ * ( ~ ) --> NOT
+ * ( ^ ) --> XOR
+ * ( << ) --> Left shift
+ * ( >> ) -->Right shift
+ * ( >>> ) --> unsigned right shift 
+ */
+
