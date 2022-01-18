feb18038353ea45d9adb2f1bb5c21fb2686811df
= <=>
2022-01-18 15:30:37 +0600

    Complete chapter -2

---
+++ chapter-2/assignment.js
@@ -0,0 +1,22 @@
+//JavaScript Assignment Operators
+
+/**
+ * Assignment operators assign values to JavaScript variables.
+ * 
+ */
+/*
+ Operator	Example	Same As
+ =	x = y	x = y
+ +=	x += y	x = x + y
+ -=	x -= y	x = x - y
+ *=	x *= y	x = x * y
+ /=	x /= y	x = x / y
+ %=	x %= y	x = x % y
+ <<=	x <<= y	x = x << y
+ >>=	x >>= y	x = x >> y
+ >>>=	x >>>= y	x = x >>> y
+ &=	x &= y	x = x & y
+ ^=	x ^= y	x = x ^ y
+ |=	x |= y	x = x | y
+ **=	x **= y	x = x ** y
+ */
